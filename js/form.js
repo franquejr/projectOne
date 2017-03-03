7e70cabd3fe717d16e13290f41ed8be4e0f298d5
Francisco Roman <froman2@hawk.iit.edu>
2017-03-03 12:22:45 -0600

    added image element to innerhtml

--- js/form.js
+++ js/form.js
@@ -9,6 +9,8 @@ console.log('Dom loaded');
     var email = document.getElementById('email').value.trim();
     var results = document.getElementById('results');
     var form = document.getElementById('sign-up');
+    var pic = document.createElement('img');
+
       if (name ==='' || email ===''){
         alert('name or email fields cannot be empty');
       }//end of name if statement
@@ -16,8 +18,12 @@ console.log('Dom loaded');
         alert('phone number entry is not a number');
       }//end of email if statement
       else{
-         results.innerHTML = '<p>Entry submitted</p><p>Thank you</p>'; 
-          form.innerHTML = " ";      
+        pic.src = 'images/Good-luck.gif';
+        pic.setAttribute('alt', 'thumbs up picture');
+        results.innerHTML = '<p>Entry submitted</p><p>Thank you</p>';
+        results.appendChild(pic);
+          
+         form.innerHTML = " ";      
       }//end of else 
     };//end of submitClicked funtion
 
